ebd8f3b8020eb4cb2cbef37c0456c57fc2b5d05c
okodee <okodee@naver.com>
2025-09-11 11:24:50 +0900

    feat: User Entity, Repository 추가

---
+++ src/main/java/org/ikuzo/otboo/domain/user/entity/User.java
@@ -0,0 +1,62 @@
+package org.ikuzo.otboo.domain.user.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.Table;
+import java.time.Instant;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.ikuzo.otboo.global.base.BaseUpdatableEntity;
+
+@Entity
+@Table(name = "users")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class User extends BaseUpdatableEntity {
+
+    @Column(length = 100, nullable = false, unique = true)
+    private String email;
+
+    @Column(length = 20, nullable = false)
+    private String name;
+
+    @Column(length = 60, nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private Gender gender;
+
+    @Column
+    private Instant birthDate;
+
+    @Column
+    private double latitude;
+
+    @Column
+    private double longitude;
+
+    @Column
+    private int x;
+
+    @Column
+    private int y;
+
+    @Column(length = 255)
+    private String locationNames;
+
+    @Column(nullable = false)
+    private int temperatureSensitivity;
+
+    @Column(columnDefinition = "TEXT")
+    private String profileImageUrl;
+
+    @Column(nullable = false)
+    private boolean locked;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+}
