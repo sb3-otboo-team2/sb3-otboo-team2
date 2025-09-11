11d04cf7758f90460b510307f15503acac0a7b99
pureod <sjo06102@gmail.com>
2025-09-11 11:44:25 +0900

    feat: 추천 관련 엔티티 생성

---
+++ src/main/java/org/ikuzo/otboo/domain/recommend/entity/Recommend.java
@@ -0,0 +1,30 @@
+package org.ikuzo.otboo.domain.recommend.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.ikuzo.otboo.global.base.BaseEntity;
+
+@Builder
+@Entity
+@Table(name = "recommends")
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Recommend extends BaseEntity {
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "weather_id", nullable = false)
+    private Weather weather;
+}
---
+++ src/main/java/org/ikuzo/otboo/domain/recommend/entity/RecommendationClothes.java
@@ -0,0 +1,31 @@
+package org.ikuzo.otboo.domain.recommend.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.ikuzo.otboo.domain.clothes.entity.Clothes;
+import org.ikuzo.otboo.global.base.BaseEntity;
+
+@Builder
+@Entity
+@Table(name = "recommendation_clothes")
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class RecommendationClothes extends BaseEntity {
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "clothes_id", nullable = false)
+    private Clothes clothes;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "recommend_id", nullable = false)
+    private Recommend recommend;
+}
