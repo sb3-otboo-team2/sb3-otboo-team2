ea56e6b6460d27d7dc8505b3e7496a555621bae2
barrackkk <kgmlwns904@gmail.com>
2025-09-15 13:42:10 +0900

    feat: 좌표가 설정된 활성 사용자들을 특정하는 메서드 추가

--- src/main/java/org/ikuzo/otboo/domain/user/repository/UserRepository.java
+++ src/main/java/org/ikuzo/otboo/domain/user/repository/UserRepository.java
@@ -1,8 +1,10 @@
 package org.ikuzo.otboo.domain.user.repository;
 
+import java.util.List;
 import java.util.UUID;
 import org.ikuzo.otboo.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, UUID> {
+    List<User> findByLockedFalseAndLatitudeIsNotNullAndLongitudeIsNotNull();
 }
