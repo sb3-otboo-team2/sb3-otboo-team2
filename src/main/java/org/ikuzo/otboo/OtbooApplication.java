97c9f9b10a1189237dc915ce7fa755ccbf0cf342
barrackkk <kgmlwns904@gmail.com>
2025-09-15 14:03:57 +0900

    fix: Application에 @ConfigurationPropertiesScan 추가로 외부 설정 바인딩 실패 수정

--- src/main/java/org/ikuzo/otboo/OtbooApplication.java
+++ src/main/java/org/ikuzo/otboo/OtbooApplication.java
@@ -2,12 +2,14 @@ package org.ikuzo.otboo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan(basePackages = "org.ikuzo.otboo")
 public class OtbooApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(OtbooApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(OtbooApplication.class, args);
+    }
 
 }
