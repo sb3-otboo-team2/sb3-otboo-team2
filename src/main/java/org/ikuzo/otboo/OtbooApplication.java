dac2db9cc4aeb4ba15f11c1b65295cc09ac193ea
barrackkk <kgmlwns904@gmail.com>
2025-09-15 14:19:52 +0900

    fix: 불필요한 @ConfigurationPropertiesScan 제거 및 부팅 오류 해소

--- src/main/java/org/ikuzo/otboo/OtbooApplication.java
+++ src/main/java/org/ikuzo/otboo/OtbooApplication.java
@@ -2,10 +2,8 @@ package org.ikuzo.otboo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
-@ConfigurationPropertiesScan(basePackages = "org.ikuzo.otboo")
 public class OtbooApplication {
 
     public static void main(String[] args) {
