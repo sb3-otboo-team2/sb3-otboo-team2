2911a76704b841ce9aca85410bb9de8eed039120
barrackkk <kgmlwns904@gmail.com>
2025-09-15 13:39:57 +0900

    feat: 직전에 저장된 예보와 비교하는 메서드 추가

--- src/main/java/org/ikuzo/otboo/domain/weather/repository/WeatherRepository.java
+++ src/main/java/org/ikuzo/otboo/domain/weather/repository/WeatherRepository.java
@@ -1,8 +1,11 @@
 package org.ikuzo.otboo.domain.weather.repository;
 
+import java.util.Optional;
 import java.util.UUID;
+import org.ikuzo.otboo.domain.user.entity.User;
 import org.ikuzo.otboo.domain.weather.entity.Weather;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface WeatherRepository extends JpaRepository<Weather, UUID> {
+    Optional<Weather> findTop1ByUserOrderByForecastAtDesc(User user);
 }
