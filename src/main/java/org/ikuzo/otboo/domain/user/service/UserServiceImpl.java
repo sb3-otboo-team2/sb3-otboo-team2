0e8eacb58998559eda85829ef8dc42a68a16d7d2
okodee <okodee@naver.com>
2025-09-15 14:30:03 +0900

    feat: Add password encoding logic

--- src/main/java/org/ikuzo/otboo/domain/user/service/UserServiceImpl.java
+++ src/main/java/org/ikuzo/otboo/domain/user/service/UserServiceImpl.java
@@ -8,6 +8,7 @@ import org.ikuzo.otboo.domain.user.entity.User;
 import org.ikuzo.otboo.domain.user.exception.UserAlreadyExistsException;
 import org.ikuzo.otboo.domain.user.mapper.UserMapper;
 import org.ikuzo.otboo.domain.user.repository.UserRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -17,6 +18,7 @@ import org.springframework.transaction.annotation.Transactional;
 public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
     private final UserMapper userMapper;
 
     @Transactional
@@ -30,8 +32,11 @@ public class UserServiceImpl implements UserService {
             throw UserAlreadyExistsException.withEmail(email);
         }
 
+        // 비밀번호 암호화
+        String encodedPassword = passwordEncoder.encode(userCreateRequest.password());
+
         // 유저 생성 및 저장
-        User user = new User(email, userCreateRequest.name(), userCreateRequest.password());
+        User user = new User(email, userCreateRequest.name(), encodedPassword);
         userRepository.save(user);
 
         log.info("사용자 생성 완료: id={}, name={}", user.getId(), user.getName());
