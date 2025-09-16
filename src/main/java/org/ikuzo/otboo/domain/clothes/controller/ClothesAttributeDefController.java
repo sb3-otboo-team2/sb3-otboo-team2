989e48a569e65c545c11e4c70b4929a7d9a6a4af
pureod <sjo06102@gmail.com>
2025-09-16 16:01:45 +0900

    feat: 의상 속성 수정 기능 구현

--- src/main/java/org/ikuzo/otboo/domain/clothes/controller/ClothesAttributeDefController.java
+++ src/main/java/org/ikuzo/otboo/domain/clothes/controller/ClothesAttributeDefController.java
@@ -1,14 +1,18 @@
 package org.ikuzo.otboo.domain.clothes.controller;
 
 import jakarta.validation.Valid;
+import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.ikuzo.otboo.domain.clothes.controller.api.ClothesAttributeDefApi;
 import org.ikuzo.otboo.domain.clothes.dto.ClothesAttributeDefDto;
 import org.ikuzo.otboo.domain.clothes.dto.request.ClothesAttributeDefCreateRequest;
+import org.ikuzo.otboo.domain.clothes.dto.request.ClothesAttributeDefUpdateRequest;
 import org.ikuzo.otboo.domain.clothes.service.ClothesAttributeDefService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -36,4 +40,18 @@ public class ClothesAttributeDefController implements ClothesAttributeDefApi {
         return ResponseEntity.status(HttpStatus.CREATED).body(dto);
     }
 
+    @PatchMapping("/{definitionId}")
+    @Override
+    public ResponseEntity<ClothesAttributeDefDto> update(
+        @PathVariable UUID definitionId,
+        @RequestBody @Valid ClothesAttributeDefUpdateRequest request
+    ) {
+        log.info("[Controller] 속성 수정 요청 - name: {}", request.name());
+
+        ClothesAttributeDefDto dto = clothesAttributeDefService.update(definitionId, request);
+
+        log.info("[Controller] 속성 수정 완료 - name: {}", dto.name());
+
+        return ResponseEntity.status(HttpStatus.OK).body(dto);
+    }
 }
