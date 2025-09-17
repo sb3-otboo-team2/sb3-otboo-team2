afb6db6aaa9a4b3e157b7237f4fed6d3d541692d
okodee <okodee@naver.com>
2025-09-17 11:47:57 +0900

    feat: Add OtbooUserDetailsService

--- src/main/java/org/ikuzo/otboo/domain/user/repository/UserRepository.java
+++ src/main/java/org/ikuzo/otboo/domain/user/repository/UserRepository.java
@@ -1,9 +1,12 @@
 package org.ikuzo.otboo.domain.user.repository;
 
+import java.util.Optional;
 import java.util.UUID;
 import org.ikuzo.otboo.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, UUID> {
+    Optional<User> findByName(String name);
+    Optional<User> findByEmail(String email);
     boolean existsByEmail(String email);
 }
