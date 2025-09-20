e868784000ce0d344482deadb382bc73d061103e
barrackkk <kgmlwns904@gmail.com>
2025-09-20 14:18:34 +0900

    refactor: 매핑되어있지 않는 에러코드 매핑

--- src/main/java/org/ikuzo/otboo/global/exception/GlobalExceptionHandler.java
+++ src/main/java/org/ikuzo/otboo/global/exception/GlobalExceptionHandler.java
@@ -59,14 +59,15 @@ public class GlobalExceptionHandler {
 
     private HttpStatus determineHttpStatus(OtbooException exception) {
         return switch (exception.getErrorCode()) {
-            
+
             case DUPLICATE_USER -> HttpStatus.CONFLICT;
 
             case FOLLOW_SELF_NOT_ALLOWED, FOLLOW_ALREADY_EXISTS
             , DUPLICATED_ATTRIBUTE_NAME, REQUIRED_FIELD_MISSING
             , INVALID_ATTRIBUTE_OPTION -> HttpStatus.BAD_REQUEST;
 
-            case ATTRIBUTE_NOT_FOUND, FOLLOW_NOT_FOUND, USER_NOT_FOUND -> HttpStatus.NOT_FOUND;
+            case ATTRIBUTE_NOT_FOUND, FOLLOW_NOT_FOUND, USER_NOT_FOUND, FEED_CLOTHES_NOT_FOUND, WEATHER_NOT_FOUND ->
+                HttpStatus.NOT_FOUND;
 
             default -> HttpStatus.INTERNAL_SERVER_ERROR;
         };
