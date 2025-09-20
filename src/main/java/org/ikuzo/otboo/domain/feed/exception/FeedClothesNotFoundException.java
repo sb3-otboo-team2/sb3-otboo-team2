24a8e6acaa5b5f701a8d3f53386737841c0868c4
barrackkk <kgmlwns904@gmail.com>
2025-09-20 20:23:28 +0900

    chore: 와일드카드 사용 대신 타입 명시화

--- src/main/java/org/ikuzo/otboo/domain/feed/exception/FeedClothesNotFoundException.java
+++ src/main/java/org/ikuzo/otboo/domain/feed/exception/FeedClothesNotFoundException.java
@@ -1,16 +1,17 @@
 package org.ikuzo.otboo.domain.feed.exception;
 
 import java.util.Set;
+import java.util.UUID;
 import org.ikuzo.otboo.global.exception.ErrorCode;
 
 public class FeedClothesNotFoundException extends FeedException {
 
-    private FeedClothesNotFoundException(Set<?> missingIds) {
+    private FeedClothesNotFoundException(Set<UUID> missingIds) {
         super(ErrorCode.FEED_CLOTHES_NOT_FOUND);
         addDetail("missingClothesIds", missingIds);
     }
 
-    public static FeedClothesNotFoundException withMissingIds(Set<?> missingIds) {
+    public static FeedClothesNotFoundException withMissingIds(Set<UUID> missingIds) {
         return new FeedClothesNotFoundException(missingIds);
     }
 }
