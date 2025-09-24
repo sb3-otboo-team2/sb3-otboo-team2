4c49d3504f0200407c39471dc5a71de535e0d357
barrackkk <kgmlwns904@gmail.com>
2025-09-24 15:16:42 +0900

    chore: 요청 시 로그 추가

--- src/main/java/org/ikuzo/otboo/domain/feed/controller/FeedLikeController.java
+++ src/main/java/org/ikuzo/otboo/domain/feed/controller/FeedLikeController.java
@@ -3,6 +3,7 @@ package org.ikuzo.otboo.domain.feed.controller;
 
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.ikuzo.otboo.domain.feedLike.service.FeedLikeService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/feeds/{feedId}/like")
 @RequiredArgsConstructor
+@Slf4j
 public class FeedLikeController {
 
     private final FeedLikeService feedLikeService;
@@ -21,7 +23,12 @@ public class FeedLikeController {
     public ResponseEntity<Void> create(
         @PathVariable UUID feedId
     ) {
+        log.info("[FeedLikeController] 피드 좋아요 생성 feedId={}", feedId);
+
         feedLikeService.create(feedId);
+
+        log.info("[FeedLikeController] 피드 좋아요 완료 feedId={}", feedId);
+
         return ResponseEntity.noContent().build();
     }
 }
