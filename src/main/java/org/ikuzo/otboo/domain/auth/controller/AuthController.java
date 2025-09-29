784ab4a401e547802fadfe166f265e7ec52100ea
okodee <okodee@naver.com>
2025-09-29 15:31:11 +0900

    feat: Implement reset password feature

--- src/main/java/org/ikuzo/otboo/domain/auth/controller/AuthController.java
+++ src/main/java/org/ikuzo/otboo/domain/auth/controller/AuthController.java
@@ -2,16 +2,24 @@ package org.ikuzo.otboo.domain.auth.controller;
 
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.validation.Valid;
+import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.ikuzo.otboo.domain.auth.dto.JwtInformation;
+import org.ikuzo.otboo.domain.auth.dto.ResetPasswordRequest;
 import org.ikuzo.otboo.domain.auth.service.AuthService;
 import org.ikuzo.otboo.domain.auth.dto.JwtDto;
+import org.ikuzo.otboo.domain.user.dto.UserDto;
+import org.ikuzo.otboo.domain.user.dto.UserRoleUpdateRequest;
 import org.ikuzo.otboo.global.security.JwtTokenProvider;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -40,5 +48,14 @@ public class AuthController {
         return ResponseEntity
             .status(HttpStatus.OK)
             .body(body);
+    }
+
+    @PostMapping("/reset-password")
+    public ResponseEntity<Void> resetPassword(
+        @RequestBody @Valid ResetPasswordRequest request
+    ) {
+        log.info("비밀번호 초기화 요청: email={}", request.email());
+        authService.resetPassword(request);
+        return ResponseEntity.noContent().build();
     }
 }
