0f716afdd350ea1131dd123e23a2c8449ee95fa9
pureod <sjo06102@gmail.com>
2025-10-21 10:01:35 +0900

    feat: 의상 속성 수정 시, 정해진 속성 값으로만 수정 가능하도록 검증

--- src/main/java/org/ikuzo/otboo/domain/clothes/entity/ClothesAttribute.java
+++ src/main/java/org/ikuzo/otboo/domain/clothes/entity/ClothesAttribute.java
@@ -13,6 +13,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.ikuzo.otboo.domain.clothes.exception.InvalidAttributeOptionException;
 import org.ikuzo.otboo.domain.clothes.exception.MissingRequiredFieldException;
 import org.ikuzo.otboo.global.base.BaseUpdatableEntity;
 
@@ -48,6 +49,17 @@ public class ClothesAttribute extends BaseUpdatableEntity {
     public void updateOptionValue(String newValue) {
         if (newValue == null || newValue.isBlank()) {
             throw new MissingRequiredFieldException("optionValue 비어있음");
+        }
+        // 선택지 검증(있을 때만)
+        if (definition != null && definition.getOptions() != null && !definition.getOptions()
+            .isEmpty()) {
+            boolean allowed = definition.getOptions().stream()
+                .map(AttributeOption::getValue)
+                .anyMatch(newValue::equals);
+            if (!allowed) {
+                throw new InvalidAttributeOptionException(
+                    "해당 속성에서 선택 불가한 옵션 값 입니다. definition=" + definition.getName() + ", 입력값=" + newValue);
+            }
         }
         this.optionValue = newValue;
     }
